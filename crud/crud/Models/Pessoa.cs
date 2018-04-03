eb26bd2072c63fdc4bdc716f92e60f0181999e1b
Robson Alves <robsonalvesdelima1@hotmail.com>
2018-04-03 14:04:31 -0300

    primeiro commit

---
+++ crud/crud/Models/Pessoa.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace crud.Models
+{
+    public class Pessoa
+    {   [Key] 
+        public int Codigo { get; set; }
+        public String Nome { get; set; }
+        public String Email { get; set; }
+
+    }
+}
